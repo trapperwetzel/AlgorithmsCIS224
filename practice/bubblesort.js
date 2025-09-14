338ce1ce25470f1a9be3dfb35285c5213128d3ce
t_coding <trapperwetzel@gmail.com>
2025-09-14 15:53:46 -0500

    insertion sort

--- practice/bubblesort.js
+++ practice/bubblesort.js
@@ -4,13 +4,14 @@
 const bubblesort = (numbers) => {
 
     for(let i = 0; i < numbers.length; i++){
-        
-        for(let j = 0; j < numbers.length - 1 - i; j++){
+        for(let j =0; j < numbers.length - 1 - i; j++){
             if(numbers[j] > numbers[j+1]){
                 const temp = numbers[j];
-                numbers[j] = numbers[j + 1];
-                numbers[j + 1] = temp;
+                numbers[j] = numbers[j+1];
+                numbers[j+1] = temp; 
             }
+
+
         }
     }
 
---
+++ practice/insertionsort.js
@@ -0,0 +1,26 @@
+
+
+// given a unsorted array return a sorted array using insertion sort
+
+
+const insertionsort = (numbers) => {
+    for(let i = 1; i < numbers.length; i++){
+        for(let j = i; j > 0; j--){
+            if(numbers[j] < numbers[j-1]){
+                [numbers[j],numbers[j-1]] = [numbers[j-1], numbers[j]] //swap
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+const testarray = [9,1,4,3,8,5,2,7,10,6];
+insertionsort(testarray);
+console.log(testarray);

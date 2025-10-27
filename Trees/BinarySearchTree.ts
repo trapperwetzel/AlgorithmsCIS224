97a3b6214ebadd9faf8c61d8a87e1fcf29b72a62
t_coding <trapperwetzel@gmail.com>
2025-10-26 19:12:19 -0500

    Binary search tree in Typescript

--- Trees/BinarySearchTree.ts
+++ Trees/BinarySearchTree.ts
@@ -13,7 +13,6 @@ class Node<T> {
   printValue() {
     console.log("--------------");
     console.log(this.value);
-    console.log("--------------");
   }
 }
 
@@ -26,6 +25,7 @@ stringNode.rightNode = stringNode2;
 
 class SimpleBinaryTree<T> {
   root?: Node<T>;
+
 
   insert(value: T): void {
     if (!this.root) {
@@ -46,13 +46,92 @@ class SimpleBinaryTree<T> {
       throw new Error("value is already in the tree!");
     }
   }
+
+  search(value: T) {
+    if (!this.root) {
+      throw new Error("No nodes are inside the tree");
+    } else {
+      return this.searchHelper(value, this.root);
+    }
+  }
+
+  searchHelper(value: T, node: Node<T>): Node<T> {
+    if (value == node.value) {
+      return node;
+    } else if (value < node.value && node.leftNode) {
+      return this.searchHelper(value, node.leftNode);
+    } else if (value > node.value && node.rightNode) {
+      return this.searchHelper(value, node.rightNode);
+    } else {
+      throw new Error("Can not find node inside tree");
+    }
+  }
+
+  preOrderTraversal(): void {
+    const preOrderHelper = (helperNode?: Node<T>) => {
+      if (helperNode) {
+        helperNode.printValue();
+        preOrderHelper(helperNode.leftNode)
+        preOrderHelper(helperNode.rightNode);
+      }
+    }
+    if (this.root) {
+      preOrderHelper(this.root);
+    }
+  }
+
+  postOrderTraversal(): void {
+    const postOrderHelper = (helperNode?: Node<T>) => {
+      if (!helperNode) { return; }
+      postOrderHelper(helperNode.leftNode)
+      postOrderHelper(helperNode.rightNode);
+      helperNode.printValue();
+    }
+    if (this.root) {
+      postOrderHelper(this.root);
+    }
+  }
+
+  lvs(target: T) {
+    let currentNode = this.root;
+    let candidateNode;
+
+    while (currentNode) {
+      if (currentNode.value < target) {
+        candidateNode = currentNode.value;
+        currentNode = currentNode.rightNode;
+      } else {
+        currentNode = currentNode.leftNode;
+      }
+    }
+    return candidateNode;
+  }
+
 }
+
+
+
+
+
 
 const testTree = new SimpleBinaryTree<number>();
 testTree.insert(25);
 testTree.insert(10);
-testTree.insert(3);
-testTree.insert(2);
 testTree.insert(33);
-testTree.insert(1);
-console.log(testTree);
+testTree.insert(27);
+testTree.insert(5);
+testTree.insert(13);
+testTree.insert(45);
+const findNode33 = testTree.search(33);
+const findNode10 = testTree.search(10);
+console.log("find node 33: ", findNode33);
+console.log("find node 10: ", findNode10);
+console.log("preOrderTraversal");
+testTree.preOrderTraversal();
+console.log("postOrderTraversal");
+testTree.postOrderTraversal();
+const testnum = testTree.lvs(25);
+console.log("NUMBER:", testnum);
+
+
+
